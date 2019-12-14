0c9ce48ff16084592f7dafe9867a40146e509ec2
mohammed oubia <mohamed4oubia@gmail.com>
2019-12-14 18:36:10 +0100

    end

--- ex.c
+++ ex.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
-#include <stdilib>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
+intf("%d\n",p);
+}
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+int langueur(char *p){
+    int i,compt,n;
+    i=0;compt=0;
+    while (*(p+i)<n)
+    {
+        i++;
+        compt++;
+        printf("%d\n",compt);
+    }
+}
+int main()
+{
+    int n;
+    char *p;
+    p=(int*)malloc(n*sizeof(char));
+    langueur(p);
+}
+
