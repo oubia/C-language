b12e698ffd4f24f734430263183e47d5d258baba
mohammed oubia <mohamed4oubia@gmail.com>
2019-12-15 14:49:14 +0100

    end

--- ex.c
+++ ex.c
@@ -2,26 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-intf("%d\n",p);
-}
+// intf("%d\n",p);
+// }
 // #include <stdio.h>
 // #include <stdlib.h>
 // #include <string.h>
-int langueur(char *p){
-    int i,compt,n;
-    i=0;compt=0;
-    while (*(p+i)<n)
-    {
-        i++;
-        compt++;
-        printf("%d\n",compt);
-    }
-}
+// int langueur(char *p){
+//     int i,compt,n;
+//     i=0;compt=0;
+//     while (*(p+i)<n)
+//     {
+//         i++;
+//         compt++;
+//         printf("%d\n",compt);
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     char *p;
+//     p=(int*)malloc(n*sizeof(char));
+//     langueur(p);
+// }
+
 int main()
 {
-    int n;
-    char *p;
-    p=(int*)malloc(n*sizeof(char));
-    langueur(p);
-}
+    int x,D,x1,x2;
+    double a,b,c;
 
+    a*(pow(x,2))+b*x+c == 0;
+    D=pow(b,2)-4*a*c;
+    if(D>0){
+        x1=(-b-sqrt(D))/2*a;
+        x2=(-b+sqrt(D))/2*a;
+        printf("le solutions et x1:%d x2 : %d\n",x1,x2);
+    }
+    else if (D=0){
+        x=-b/2*a;
+        printf("la solution et x: %d\n",x);
+    }
+    else 
+    printf("no solution\n");
+}
