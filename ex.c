3063fcb2b25894b4bafa6e0737a3717b3c1bba2c
mohammed oubia <mohamed4oubia@gmail.com>
2019-12-15 17:37:50 +0100

    end

--- ex.c
+++ ex.c
@@ -25,22 +25,49 @@
 //     langueur(p);
 // }
 
+// int main()
+// {
+//     int x,D,x1,x2;
+//     double a,b,c;
+
+//     a*(pow(x,2))+b*x+c == 0;
+//     D=pow(b,2)-4*a*c;
+//     if(D>0){
+//         x1=(-b-sqrt(D))/2*a;
+//         x2=(-b+sqrt(D))/2*a;
+//         printf("le solutions et x1:%d x2 : %d\n",x1,x2);
+//     }
+//     else if (D=0){
+//         x=-b/2*a;
+//         printf("la solution et x: %d\n",x);
+//     }
+//     else 
+//     printf("no solution\n");
+// }
+
 int main()
 {
-    int x,D,x1,x2;
-    double a,b,c;
-
-    a*(pow(x,2))+b*x+c == 0;
-    D=pow(b,2)-4*a*c;
-    if(D>0){
-        x1=(-b-sqrt(D))/2*a;
-        x2=(-b+sqrt(D))/2*a;
-        printf("le solutions et x1:%d x2 : %d\n",x1,x2);
-    }
-    else if (D=0){
-        x=-b/2*a;
-        printf("la solution et x: %d\n",x);
-    }
-    else 
-    printf("no solution\n");
+    int n,i,d;
+    n=0;
+    printf("donner le nbr");
+    scanf("%d",&n);
+    for ( i = n-1; i < n+9; i++)
+    {
+        d=n+i;
+        printf("les nombres sons :%d\n",d);
+    }  
+}
+int main()
+{
+    int n,i,d;
+    n=0;
+    printf("donner le nbr");
+    scanf("%d",&n);
+    i=n-1;
+    while (i<n+9)
+    {
+        d=n+i;
+        printf("les nombres sons :%d\n",d);
+        i++;
+    } 
 }
